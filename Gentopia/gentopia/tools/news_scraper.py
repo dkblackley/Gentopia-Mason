8bdb589dc1b1aa298347e41df4311fd826c5a188
yelnat <yelnat@debian-x570>
2024-10-11 10:30:11 -0400

    temp commit

--- Gentopia/gentopia/tools/news_scraper.py
+++ Gentopia/gentopia/tools/news_scraper.py
@@ -111,7 +111,7 @@ def get_article_content(url):
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
         }
 
-        time.sleep(5) # Stop 429 too many requests
+        time.sleep(1) # Stop 429 too many requests
         response = requests.get(url, headers=headers)
         print(url)
         print(response)
